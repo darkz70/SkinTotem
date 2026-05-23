e00fe19149bbe9ac6d266086ce3f597582af316f
darkz70 <d91313250@gmail.com>
2026-05-23 13:45:20 +0200

    Update ItemRendererMixin.java

--- src/main/java/spichka/skintotem/mixin/ItemRendererMixin.java
+++ src/main/java/spichka/skintotem/mixin/ItemRendererMixin.java
@@ -5,30 +5,22 @@ import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
 
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.NbtComponent;
-
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.UUID;
-
-import spichka.skintotem.skin.SkinManager;
-
 @Mixin(ItemRenderer.class)
 public class ItemRendererMixin {
 
     @Inject(
-            method = "render",
-            at = @At("TAIL"),
+            method = "renderItem",
+            at = @At("HEAD"),
             require = 0
     )
-    private void skintotem$cacheSkin(
+    private void skintotem$render(
             ItemStack stack,
             ModelTransformationMode mode,
             boolean leftHanded,
@@ -39,26 +31,5 @@ public class ItemRendererMixin {
             CallbackInfo ci
     ) {
 
-        if (stack == null || stack.isEmpty()) {
-            return;
-        }
-
-        NbtComponent data =
-                stack.get(DataComponentTypes.CUSTOM_DATA);
-
-        if (data == null) {
-            return;
-        }
-
-        NbtCompound nbt = data.copyNbt();
-
-        if (!nbt.containsUuid("Owner")) {
-            return;
-        }
-
-        UUID owner = nbt.getUuid("Owner");
-
-        // SAFE CACHE ONLY
-        SkinManager.getSkin(owner);
     }
 }
