9b58a06a6a7028a86823da1cc5a5fb6735b7c3d2
darkz70 <d91313250@gmail.com>
2026-05-23 20:42:26 +0200

    Update SkinTotemClient.java

--- src/main/java/spichka/skintotem/client/SkinTotemClient.java
+++ src/main/java/spichka/skintotem/client/SkinTotemClient.java
@@ -2,14 +2,13 @@ package spichka.skintotem.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
-import net.minecraft.item.Items;
 
 public class SkinTotemClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
         BuiltinItemRendererRegistry.INSTANCE.register(
-                Items.TOTEM_OF_UNDYING,
+                YourModItems.TOTEM_ITEM,
                 new SkinTotemItemRenderer()
         );
     }
