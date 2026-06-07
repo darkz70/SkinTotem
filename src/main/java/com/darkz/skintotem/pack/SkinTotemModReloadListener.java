756e697e54e5f35687924ed79c4ec7d3ee211d7b
darkz70 <d91313250@gmail.com>
2026-06-07 22:16:13 +0200

    Update SkinTotemModReloadListener.java

--- src/main/java/com/darkz/skintotem/pack/SkinTotemModReloadListener.java
+++ src/main/java/com/darkz/skintotem/pack/SkinTotemModReloadListener.java
@@ -2,13 +2,13 @@ package com.darkz.skintotem.pack;
 
 import java.util.concurrent.*;
 import com.darkz.skintotem.atlas.manager.*;
-import net.minecraft.resource.*;
+import net.minecraft.resources.*;
 import net.minecraft.util.*;
 import net.fabricmc.fabric.api.resource.*;
 import com.darkz.skintotem.SkinTotemMod;
 import com.darkz.skintotem.model.bb.manager.BlockBenchModelManager;
 import com.darkz.skintotem.tag.manager.TagsManager;
-import net.minecraft.util.profiler.*;
+import net.minecraft.util.profiler.Profilers;
 
 //? if >=1.21.9 {
 import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
@@ -25,7 +25,7 @@ public class SkinTotemModReloadListener implements /*? if >=1.21.9 {*/ ResourceR
 	}
 
 	/*? if <=1.21.8 {*//*@Override*//*?}*/
-	public /*? if >=1.21.9 {*/ static /*?}*/ Identifier getFabricId() {
+	public /*? if >=1.21.9 {*/ static /*?}*/ ResourceLocation getFabricId() {
 		return SkinTotemMod.id("%s-reload-listener".formatted(SkinTotemMod.MOD_ID));
 	}
 
@@ -39,39 +39,5 @@ public class SkinTotemModReloadListener implements /*? if >=1.21.9 {*/ ResourceR
 			profiler.pop();
 		}, applyExecutor);
 	}
-	//?} elif >=1.21.2 {
-	/*@Override
-	public CompletableFuture<Void> reload(Synchronizer synchronizer, ResourceManager manager, Executor prepareExecutor, Executor applyExecutor) {
-		return synchronizer.whenPrepared(Unit.INSTANCE).thenRunAsync(() -> {
-			Profiler profiler = Profilers.get();
-			profiler.push("listener");
-			this.reloadStuff(synchronizer, manager, prepareExecutor, applyExecutor);
-			profiler.pop();
-		}, applyExecutor);
-	}
-	*///?} else {
-	/*@Override
-	public CompletableFuture<Void> reload(ResourceReloader.Synchronizer synchronizer, ResourceManager manager, Profiler prepareProfiler, Profiler applyProfiler, Executor prepareExecutor, Executor applyExecutor) {
-		return synchronizer.whenPrepared(Unit.INSTANCE).thenRunAsync(() -> {
-			applyProfiler.startTick();
-			applyProfiler.push("listener");
-			this.reloadStuff(synchronizer, manager, prepareExecutor, applyExecutor);
-			applyProfiler.pop();
-			applyProfiler.endTick();
-		}, applyExecutor);
-	}
-
-	*///?}
-
-	private void reloadStuff(Synchronizer synchronizer, ResourceManager resourceManager, Executor prepareExecutor, Executor applyExecutor) {
-		this.reloadAtlas(synchronizer, prepareExecutor, applyExecutor);
-		BlockBenchModelManager.reload(resourceManager);
-		TotemDollModelFinder.reload(resourceManager);
-		TagsManager.reloadCustomModelIdsTags();
-	}
-
-	private void reloadAtlas(Synchronizer synchronizer, Executor prepareExecutor, Executor applyExecutor) {
-		SkinTotemModAtlasSpriteManager.reload();
-		SkinTotemModAtlasManager.stitchAndUpdate(SkinTotemModAtlasSpriteManager.getSprites(), synchronizer, prepareExecutor, applyExecutor, null);
-	}
-}
+	//?} elif
+
