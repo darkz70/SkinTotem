ab17a802d4416cfd75c7fba137ea015b258d3e07
darkz69i42k <d91313250@gmail.com>
2026-07-09 10:22:14 +0200

    Update SkinTotemCommand.java

--- src/main/java/com/darkz/skintotem/client/command/SkinTotemCommand.java
+++ src/main/java/com/darkz/skintotem/client/command/SkinTotemCommand.java
@@ -3,7 +3,14 @@ package com.darkz.skintotem.client.command;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import com.darkz.skintotem.skin.provider.extended.TLauncherSkinProvider;
+import com.darkz.skintotem.skin.provider.extended.ElyBySkinProvider;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommands.literal;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommands.argument;
@@ -35,21 +42,31 @@ public class SkinTotemCommand {
     }
 
     public static LiteralArgumentBuilder<FabricClientCommandSource> getTlCommand() {
-        return literal("tl").executes(ctx -> {
-            ctx.getSource().sendFeedback(
-                Component.literal(P + "§aUsing TLauncher skin source")
-            );
-            return 1;
-        });
+        return literal("tl")
+            .then(argument("nickname", StringArgumentType.word())
+                .executes(ctx -> renameHeldTotem(ctx, TLauncherSkinProvider.PREFIX + StringArgumentType.getString(ctx, "nickname"))));
     }
 
     public static LiteralArgumentBuilder<FabricClientCommandSource> getElyCommand() {
-        return literal("ely").executes(ctx -> {
-            ctx.getSource().sendFeedback(
-                Component.literal(P + "§aUsing Ely.by skin source")
-            );
-            return 1;
-        });
+        return literal("ely")
+            .then(argument("nickname", StringArgumentType.word())
+                .executes(ctx -> renameHeldTotem(ctx, ElyBySkinProvider.PREFIX + StringArgumentType.getString(ctx, "nickname"))));
+    }
+
+    private static int renameHeldTotem(com.mojang.brigadier.context.CommandContext<FabricClientCommandSource> ctx, String name) {
+        Minecraft mc = Minecraft.getInstance();
+        Player player = mc.player;
+        if (player == null) return 0;
+
+        ItemStack stack = player.getMainHandItem();
+        if (stack.isEmpty() || !stack.is(Items.TOTEM_OF_UNDYING)) {
+            ctx.getSource().sendError(Component.literal("§cHold a Totem of Undying in your main hand!"));
+            return 0;
+        }
+
+        stack.set(DataComponents.CUSTOM_NAME, Component.literal(name));
+        ctx.getSource().sendFeedback(Component.literal(P + "§aSkin totem set to: §f" + name));
+        return 1;
     }
 
     public static LiteralArgumentBuilder<FabricClientCommandSource> getUrlCommand() {
@@ -71,8 +88,8 @@ public class SkinTotemCommand {
                 P + "§7Commands:\n" +
                 "  §f/skintotem info\n" +
                 "  §f/skintotem refresh\n" +
-                "  §f/skintotem tl\n" +
-                "  §f/skintotem ely\n" +
+                "  §f/skintotem tl <nickname>\n" +
+                "  §f/skintotem ely <nickname>\n" +
                 "  §f/skintotem url <url>\n" +
                 "  §f/skintotem credits"
             ));
