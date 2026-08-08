fd4448202cca3bde81d1af3a0dca7db378765e20
darkz69i42k <d91313250@gmail.com>
2026-08-08 12:32:21 +0200

    Update ModModelTransformationExtension.java

--- src/main/java/com/darkz/skintotem/extension/ModModelTransformationExtension.java
+++ src/main/java/com/darkz/skintotem/extension/ModModelTransformationExtension.java
@@ -4,49 +4,49 @@ import net.minecraft.client.render.model.json.*;
 
 public class ModModelTransformationExtension {
 	
-	public static Transformation getTl(ModModelTransformation transform) {
+	public static Transformation getTl(ModelTransformation transform) {
 		return /*? >=1.21.4 {*/ transform.thirdPersonLeftHand(); /*?} else {*/ /*transform.thirdPersonLeftHand; *//*?}*/
 	}
 
-	public static Transformation getTr(ModModelTransformation transform) {
+	public static Transformation getTr(ModelTransformation transform) {
 		return /*? >=1.21.4 {*/ transform.thirdPersonRightHand(); /*?} else {*/ /*transform.thirdPersonRightHand; *//*?}*/
 	}
 
-	public static Transformation getFl(ModModelTransformation transform) {
+	public static Transformation getFl(ModelTransformation transform) {
 		return /*? >=1.21.4 {*/ transform.firstPersonLeftHand(); /*?} else {*/ /*transform.firstPersonLeftHand; *//*?}*/
 	}
 
-	public static Transformation getFr(ModModelTransformation transform) {
+	public static Transformation getFr(ModelTransformation transform) {
 		return /*? >=1.21.4 {*/ transform.firstPersonRightHand(); /*?} else {*/ /*transform.firstPersonRightHand; *//*?}*/
 	}
 
-	public static Transformation getHead(ModModelTransformation transform) {
+	public static Transformation getHead(ModelTransformation transform) {
 		return /*? >=1.21.4 {*/ transform.head(); /*?} else {*/ /*transform.head; *//*?}*/
 	}
 
-	public static Transformation getGui(ModModelTransformation transform) {
+	public static Transformation getGui(ModelTransformation transform) {
 		return /*? >=1.21.4 {*/ transform.gui(); /*?} else {*/ /*transform.gui; *//*?}*/
 	}
 
-	public static Transformation getGround(ModModelTransformation transform) {
+	public static Transformation getGround(ModelTransformation transform) {
 		return /*? >=1.21.4 {*/ transform.ground(); /*?} else {*/ /*transform.ground; *//*?}*/
 	}
 
-	public static Transformation getFixed(ModModelTransformation transform) {
+	public static Transformation getFixed(ModelTransformation transform) {
 		return /*? >=1.21.4 {*/ transform.fixed(); /*?} else {*/ /*transform.fixed; *//*?}*/
 	}
 
 	//? if >=1.21.9 {
-	public static Transformation getOnShelf(ModModelTransformation transform) {
+	public static Transformation getOnShelf(ModelTransformation transform) {
 		return transform.fixedFromBottom();
 	}
 	//?}
 
-//	public static ModModelTransformation getBlockBenchedModModelTransformation(ModModelTransformation transform) {
-//		return ModModelTransformation.of(-getPivotX(transform), -getPivotY(transform), getPivotZ(transform), getPitch(transform), getYaw(transform), getRoll(transform));
+//	public static ModelTransformation getBlockBenchedModModelTransformation(ModelTransformation transform) {
+//		return ModelTransformation.of(-getPivotX(transform), -getPivotY(transform), getPivotZ(transform), getPitch(transform), getYaw(transform), getRoll(transform));
 //	}
 //
-//	public static String asString(ModModelTransformation transform) {
+//	public static String asString(ModelTransformation transform) {
 //		return "%s %s %s | %s %s %s".formatted(getPivotX(transform), getPivotY(transform), getPivotZ(transform), getPitch(transform), getYaw(transform), getRoll(transform));
 //	}
 
