c6f27a6b8382b335b48e581b45c52b462883ccc4
darkz69i42k <d91313250@gmail.com>
2026-08-08 17:18:47 +0200

    Refactor ScreenMixin for background rendering logic

--- src/main/java/com/darkz/skintotem/mixin/yacl/ScreenMixin.java
+++ src/main/java/com/darkz/skintotem/mixin/yacl/ScreenMixin.java
@@ -31,7 +31,7 @@ public abstract class ScreenMixin {
 		return YACLConfigurationScreen.notOpen(((Screen) (Object) this));
 	}
 
-	@ifyArg(method = "renderDarkening(Lnet/minecraft/client/gui/DrawContext;IIII)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;renderBackgroundTexture(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/util/Identifier;IIFFII)V"), index = 1)
+	@ModifyArg(method = "renderDarkening(Lnet/minecraft/client/gui/DrawContext;IIII)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;renderBackgroundTexture(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/util/Identifier;IIFFII)V"), index = 1)
 	private Identifier swapBackgroundTexture(Identifier original) {
 		if (YACLConfigurationScreen.notOpen(((Screen) (Object) this))) {
 			return original;
@@ -52,7 +52,7 @@ public abstract class ScreenMixin {
 	}
 
 	@Unique
-	private static final String INJECT_METHOD = /*? >=1.20.2 {*/ "renderInGameBackground" /*?} else {*/ /*"renderBackground" *//*?}*/;
+	private static final String INJECT_METHOD = /^? >=1.20.2 {^/ "renderInGameBackground" /^?} else {^/ /^"renderBackground" ^//^?}^/;
 
 	@WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;fillGradient(IIIIII)V"), method = INJECT_METHOD)
 	private void swapBackgroundGradientColor(DrawContext context, int startX, int startY, int endX, int endY, int colorStart, int colorEnd, Operation<Void> original) {
@@ -65,7 +65,7 @@ public abstract class ScreenMixin {
 
 	//? if <=1.20.1 {
 
-	/*@Shadow public abstract void renderBackground(DrawContext context);
+	/^@Shadow public abstract void renderBackground(DrawContext context);
 
 	@Inject(at = @At("HEAD"), method = "render")
 	private void renderWithBackground(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
@@ -74,7 +74,7 @@ public abstract class ScreenMixin {
 		}
 	}
 
-	*///?}
+	^///?}
 
 	*///?}
 }
