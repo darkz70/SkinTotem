a2a6322fa9d21612455d8e23c2b37952b770e11b
darkz69i42k <d91313250@gmail.com>
2026-08-08 19:55:11 +0200

    Rename method getCompatId to getCompatModId

--- src/main/java/com/darkz/skintotem/compat/citresewn/CITResewnCompatPlugin.java
+++ src/main/java/com/darkz/skintotem/compat/citresewn/CITResewnCompatPlugin.java
@@ -5,7 +5,7 @@ import com.darkz.skintotem.compat.CompatPlugin;
 public class CITResewnCompatPlugin extends CompatPlugin {
 
 	@Override
-	protected String getCompatId() {
+	protected String getCompatModId() {
 		return "citresewn";
 	}
 }
